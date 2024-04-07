2e94c8f3962b0dbff736e77ed1f9191e32d89fed
Mike Hummel <mh@mhus.de>
2024-04-07 12:38:15 +0200

    start with page header information

--- kt2l.org/src/App.tsx
+++ kt2l.org/src/App.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './kt2l-logo.svg';
 import './App.css';
-
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import pageHeader from './page-header'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return pageHeader();
 }
 
 export default App;
