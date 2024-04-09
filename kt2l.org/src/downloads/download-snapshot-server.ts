532583adfeab784d59ca06649aafe88990158fdb
Mike Hummel <mh@mhus.de>
2024-04-09 09:20:55 +0200

    Update server snapshot 2024-04-09

--- kt2l.org/src/downloads/download-snapshot-server.ts
+++ kt2l.org/src/downloads/download-snapshot-server.ts
@@ -5,7 +5,7 @@ export let download : DownloadFile = {
     title: "Server Bundled",
     description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
     href: "/snapshots/kt2l-server-2024-04-09.zip",
-    size: "10mb",
+    size: "",
     enabled: true,
     created: "2024-04-09"
 }
