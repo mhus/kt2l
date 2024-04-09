bc83fd1c79b4aca4328a790a0b5f554eb146fea2
Mike Hummel <mh@mhus.de>
2024-04-09 18:01:46 +0200

    add new templete in shell style

---
+++ kt2l.org/templates/download.ts.sh
@@ -0,0 +1,12 @@
+
+ENABLED=${ENABLED:-true}
+
+echo "import DownloadFile from '../download-ifc';"
+echo "export let download : DownloadFile = {"
+echo "    title: \"$TITLE\","
+echo "    description: \"$DESCRIPTION\","
+echo "    href: \"$HREF\","
+echo "    size: \"$SIZE\","
+echo "    enabled: $ENABLED,"
+echo "    created: \"$CREATED\""
+echo "}"
