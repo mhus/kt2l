ce082557c8a4594890c38cbe541760b7a63cf1d6
Mike Hummel <mh@mhus.de>
2024-04-09 08:51:05 +0200

    feat: add support for snapshots in git

--- kt2l.org/src/downloads/download-snapshot-server.ts
+++ kt2l.org/src/downloads/download-snapshot-server.ts
@@ -4,8 +4,8 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Server Bundled",
     description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
-    href: "/snapshots/kt2l-server-bundled-2021-09-01-12-00-00.zip",
+    href: "/snapshots/kt2l-server-2024-04-09.zip",
     size: "10mb",
     enabled: true,
-    created: "2021-09-01-12-00-00"
+    created: "2024-04-09"
 }
