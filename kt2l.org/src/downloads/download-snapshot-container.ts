e79f5c6096cc3ada78b6796b204d1fe1ac918a8e
Robot <mhus@users.noreply.github.com>
2024-04-10 21:08:58 +0000

    Update container snapshot 2024-04-10

---
+++ kt2l.org/src/downloads/download-snapshot-container.ts
@@ -0,0 +1,9 @@
+import DownloadFile from '../download-ifc';
+export let download : DownloadFile = {
+    title: "Docker Container",
+    description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
+    href: "/docs/use_container.html",
+    size: "",
+    enabled: true,
+    created: "2024-04-10"
+}
