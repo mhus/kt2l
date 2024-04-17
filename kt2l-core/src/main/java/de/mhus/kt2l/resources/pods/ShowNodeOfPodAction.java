eb2d83545763bace799d117d9f1042cd90563a62
Robot <mhus@users.noreply.github.com>
2024-04-17 13:35:10 +0200

    feat: set license header to files, optimize pom.xml

--- kt2l-core/src/main/java/de/mhus/kt2l/resources/pods/ShowNodeOfPodAction.java
+++ kt2l-core/src/main/java/de/mhus/kt2l/resources/pods/ShowNodeOfPodAction.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of kt2l-core.
+ *
+ * kt2l-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kt2l-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kt2l-core.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.mhus.kt2l.resources.pods;
 
 import com.vaadin.flow.component.icon.VaadinIcon;
--- launch/create.sh
+++ launch/create.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #
-# Copyright (C) 2020 Mike Hummel (mh@mhus.de)
+# This file is part of kt2l.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# kt2l is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+# kt2l is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# You should have received a copy of the GNU General Public License
+# along with kt2l.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 cd $(dirname $0)/..
