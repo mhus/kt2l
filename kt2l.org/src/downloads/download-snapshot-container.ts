58772679b0c1f3ebacce0a20425f9febf171bff5
Robot <mhus@users.noreply.github.com>
2024-04-18 21:58:34 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-container.ts
+++ kt2l.org/src/downloads/download-snapshot-container.ts
@@ -2,7 +2,7 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Docker Container",
     description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
-    href: "/docs/run-container",
+    href: "/docs/installation/run-container",
     size: "",
     enabled: true,
     created: "2024-04-18"
