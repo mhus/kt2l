b7db438a17875806874465f1021a2ca3729dfd16
Robot <mhus@users.noreply.github.com>
2024-04-22 17:05:10 +0200

    wip: deploy arch types

--- kt2l-desktop/launcher/create-windows-amd64.bat
+++ kt2l-desktop/launcher/create-windows-amd64.bat
@@ -24,6 +24,12 @@ jpackage  --name KT2L ^
   --main-jar kt2l-desktop-windows-amd64.jar ^
   --type exe ^
   --java-options "-XstartOnFirstThread -Dspring.profiles.active=prod" ^
+  --win-menu ^
+  --win-shortcut ^
+  --win-shortcut-prompt ^
+  --win-menu-group "KT2L" ^
+  --win-per-user-install ^
+  --icon ..\launcher\windows\kt2l.ico ^
   --vendor "www.kt2l.org"
 
 copy KT2L-1.0.exe KT2L.exe
