ee90af073874f82bc763ed40229fd2e835a5556f
Robot <mhus@users.noreply.github.com>
2024-04-25 11:54:09 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-desktop-macosx-aarch64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-macosx-aarch64.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Desktop Mac OSX (M1) Bundled",
     description: "Can be executed directly in Mac OS X M1. Java JDK 21 is included.",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-macosx-aarch64-2024-04-23.dmg",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-macosx-aarch64-2024-04-25.dmg",
     href_help: "",
     size: "",
     enabled: true,
-    created: "2024-04-23"
+    created: "2024-04-25"
 }
