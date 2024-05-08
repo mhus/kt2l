fa82f5cee12e2ca452b72640290d3ca22fa2f64d
Robot <mhus@users.noreply.github.com>
2024-05-08 08:48:23 +0000

    Build 8998843973

--- kt2l-core/src/main/java/de/mhus/kt2l/generated/DeployInfo.java
+++ kt2l-core/src/main/java/de/mhus/kt2l/generated/DeployInfo.java
@@ -4,6 +4,6 @@ package de.mhus.kt2l.generated;
 public class DeployInfo {
 
     public static final String VERSION = "0.0.1-SNAPSHOT";
-    public static final String CREATED = "2024-04-26";
+    public static final String CREATED = "2024-05-08";
 
 }
