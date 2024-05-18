0b7ab87a472810c96f2c2e1b1181137513131442
Robot <mhus@users.noreply.github.com>
2024-05-18 12:37:43 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Desktop Linux amd64 DEB",
     description: "Can be installed on debian amd64 systems",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-05-17_amd64.deb",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-05-18_amd64.deb",
     href_help: "/docs/installation/desktop#linux-deb",
     size: "",
     enabled: true,
-    created: "2024-05-17"
+    created: "2024-05-18"
 }
