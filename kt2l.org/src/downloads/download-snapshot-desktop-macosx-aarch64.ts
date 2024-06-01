14dcb539691cf9a4ba18a051ccb0b025310ad419
Robot <mhus@users.noreply.github.com>
2024-06-01 09:41:57 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-desktop-macosx-aarch64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-macosx-aarch64.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Desktop Mac OSX (M1) Bundled",
     description: "Can be executed directly in Mac OS X M1. Java JDK 21 is included.",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-macosx-aarch64-2024-05-29.dmg",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-macosx-aarch64-2024-05-31.dmg",
     href_help: "/docs/installation/desktop#mac-osx-bundle",
     size: "",
     enabled: true,
-    created: "2024-05-29"
+    created: "2024-05-31"
 }
--- kt2l.org/src/downloads/download-snapshot-server.ts
+++ kt2l.org/src/downloads/download-snapshot-server.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Server ZIP",
     description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-server-2024-05-29.zip",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-server-2024-05-31.zip",
     href_help: "/docs/installation/server",
     size: "",
     enabled: true,
-    created: "2024-05-29"
+    created: "2024-05-31"
 }
