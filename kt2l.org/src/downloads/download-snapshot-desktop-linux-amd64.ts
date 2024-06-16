4d5ad4eef564d1af1838209da728004225845612
Robot <mhus@users.noreply.github.com>
2024-06-16 14:57:49 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
@@ -4,7 +4,7 @@ export let download : DownloadFile = {
     description: "Can be installed on debian amd64 systems",
     href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-06-16_amd64.deb",
     href_help: "/docs/installation/desktop#linux-deb",
-    size: "331MB",
+    size: "329MB",
     enabled: true,
     created: "2024-06-16"
 }
--- kt2l.org/src/downloads/download-snapshot-server.ts
+++ kt2l.org/src/downloads/download-snapshot-server.ts
@@ -4,7 +4,7 @@ export let download : DownloadFile = {
     description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
     href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-server-2024-06-16.zip",
     href_help: "/docs/installation/server",
-    size: "297MB",
+    size: "295MB",
     enabled: true,
     created: "2024-06-16"
 }
