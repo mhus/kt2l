019afea0a180a80edd80fc78aab1ec0f21812cbb
Robot <mhus@users.noreply.github.com>
2024-09-18 07:00:27 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-container.ts
+++ kt2l.org/src/downloads/download-snapshot-container.ts
@@ -1,10 +1,10 @@
 import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Docker Container",
-    description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required. \"mhus/kt2l-server:snapshot-2024-09-06\"",
+    description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required. \"mhus/kt2l-server:snapshot-2024-09-18\"",
     href: "",
     href_help: "/docs/installation/container",
     size: "",
     enabled: true,
-    created: "2024-09-06"
+    created: "2024-09-18"
 }
--- kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Desktop Linux amd64 DEB",
     description: "Can be installed on debian amd64 systems",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-09-06_amd64.deb",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-09-18_amd64.deb",
     href_help: "/docs/installation/desktop#linux-deb",
-    size: "332MB",
+    size: "336MB",
     enabled: true,
-    created: "2024-09-06"
+    created: "2024-09-18"
 }
