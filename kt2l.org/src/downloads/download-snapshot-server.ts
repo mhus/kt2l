0b2d252049968417ca23d8a0fbd298b5bb4feda5
Robot <mhus@users.noreply.github.com>
2024-10-22 13:17:32 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-server.ts
+++ kt2l.org/src/downloads/download-snapshot-server.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Server ZIP",
     description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required.",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-server-2024-09-19.zip",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-server-2024-10-22.zip",
     href_help: "/docs/installation/server",
-    size: "300MB",
+    size: "306MB",
     enabled: true,
-    created: "2024-09-19"
+    created: "2024-10-22"
 }
