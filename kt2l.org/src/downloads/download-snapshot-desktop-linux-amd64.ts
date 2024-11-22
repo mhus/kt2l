0b6d2d06c390b82a74fcfe90eb50e1b9bf9dcd11
Robot <mhus@users.noreply.github.com>
2024-11-22 10:12:12 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-linux-amd64.ts
@@ -2,9 +2,9 @@ import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Desktop Linux amd64 DEB",
     description: "Can be installed on debian amd64 systems",
-    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-11-06_amd64.deb",
+    href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-linux-amd64_2024-11-22_amd64.deb",
     href_help: "/docs/installation/desktop#linux-deb",
-    size: "340MB",
+    size: "326MB",
     enabled: true,
-    created: "2024-11-06"
+    created: "2024-11-22"
 }
--- kt2l.org/src/downloads/download-snapshot-desktop-windows-amd64.ts
+++ kt2l.org/src/downloads/download-snapshot-desktop-windows-amd64.ts
@@ -4,7 +4,7 @@ export let download : DownloadFile = {
     description: "Installer for Windows amd64 systems. Java JDK 21 is included.",
     href: "https://kt2l-downloads.s3.eu-central-1.amazonaws.com/snapshots/kt2l-desktop-windows-amd64-.exe",
     href_help: "/docs/installation/desktop#windows-bundle",
-    size: "363MB",
+    size: "368MB",
     enabled: true,
-    created: "2024-11-06"
+    created: "2024-11-22"
 }
