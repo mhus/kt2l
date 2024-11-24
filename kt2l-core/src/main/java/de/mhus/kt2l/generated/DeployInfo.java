56e0e501c18bfe1d57b3db28cea644522f82f27c
Robot <mh@mhus.de>
2024-11-24 17:02:01 +0000

    chore: Release Build 11997938703

--- kt2l-core/src/main/java/de/mhus/kt2l/generated/DeployInfo.java
+++ kt2l-core/src/main/java/de/mhus/kt2l/generated/DeployInfo.java
@@ -4,6 +4,6 @@ package de.mhus.kt2l.generated;
 public class DeployInfo {
 
     public static final String VERSION = "0.0.1-SNAPSHOT";
-    public static final String CREATED = "2024-11-23";
+    public static final String CREATED = "2024-11-24";
 
 }
