2aaf9a9c91790785dc8d13758b4e20e2fdd1cf89
Robot <mhus@users.noreply.github.com>
2024-12-06 09:01:25 +0000

    Webpage

--- kt2l.org/src/downloads/download-snapshot-container.ts
+++ kt2l.org/src/downloads/download-snapshot-container.ts
@@ -1,10 +1,10 @@
 import DownloadFile from '../download-ifc';
 export let download : DownloadFile = {
     title: "Docker Container",
-    description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required. \"mhus/kt2l-server:snapshot-2024-12-05\"",
+    description: "Can be started as Server and accessed via Browser, locally also. Java JDK 21 is required. \"mhus/kt2l-server:snapshot-snapshot-2024-12-06\"",
     href: "",
     href_help: "/docs/installation/container",
     size: "",
     enabled: true,
-    created: "2024-12-05"
+    created: "snapshot-2024-12-06"
 }
